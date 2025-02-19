96d9bcf0ca1f7ec11d6ce4fa7468a9c1b446183e
Rutujagayke12345 <139562848+Rutujagayke12345@users.noreply.github.com>
2025-02-19 12:03:57 +0530

    Update CartBtn.jsx

--- CartBtn.jsx
+++ CartBtn.jsx
@@ -3,8 +3,6 @@ import { NavLink } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
 const CartBtn = () => {
-    // We get a state of addItems
-    // Write the name of the file not the function
     const state = useSelector((state)=> state.addItem)
     return (
         <>
